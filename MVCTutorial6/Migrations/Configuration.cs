2b30509cb9d39db698bbbf789880fb6d8286286a
jdiazfernandez <yesica.diaz@upm.es>
2019-03-25 12:32:10 +0100

    Agregar archivos de proyecto.

---
+++ MVCTutorial6/Migrations/Configuration.cs
@@ -0,0 +1,40 @@
+namespace MVCTutorial6.Migrations
+{
+    using MVCTutorial.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MVCTutorial6.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MVCTutorial6.Models.ApplicationDbContext context)
+        {
+            context.Products.AddOrUpdate(p => p.ProductID,
+               new Products
+               {
+                   ProductID = 1,
+                   Description = "TV",
+                   Brand = "Samsung",
+                   Quantity = 1,
+                   Price = 500,
+
+               },
+               new Products
+               {
+                   ProductID = 2,
+                   Description = "Móvil",
+                   Brand = "Apple",
+                   Quantity = 1,
+                   Price = 600,
+
+               }
+            );
+        }
+    }
+}
---
+++ MVCTutorial6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCTutorial6.Startup))]
+namespace MVCTutorial6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
